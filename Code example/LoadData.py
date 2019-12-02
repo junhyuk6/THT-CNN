b1b780ba6c90e492d98d02edbbdfd85a0241b7b2
junhyuk6 <31534248+junhyuk6@users.noreply.github.com>
2019-12-02 21:09:56 +0900

    Update LoadData.py

--- Code example/LoadData.py
+++ Code example/LoadData.py
@@ -15,7 +15,7 @@ def LoadData(nField,iniField,intervalField):
     #r.m.s of data (0 : du/dy, 1 : dw/dy, 2 : p)
     Input_std = np.empty([3])
     Input_std[0] = 66.274826292; Input_std[1] = 37.0097954241; Input_std[2] = 1.58318422533;
-    Data = np.empty([nField,nzp,nxp,4]) # [number of field, z-grids, x-grids, variables(du/dy, dw/dy, p, dT/dy)]
+    Data = np.empty([nField,nzp,nxp,4], dtype=np.float32) # [number of field, z-grids, x-grids, variables(du/dy, dw/dy, p, dT/dy)]
     for j in range(nField) :
         p = iniField + j*intervalField
         Filename = '/DLdata/' + '%05d'%(p)
